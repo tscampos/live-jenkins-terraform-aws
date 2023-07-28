fda957fe78148a0a81ca891df290b079111959de
Tércio Sodré Campos <tercio.sodre@gmail.com>
2023-07-28 14:23:01 -0300

    Subindo estrututa inicial do teste de pipeline

---
+++ jenkins/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.29.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "" # Substitua pelo seu token da DigitalOcean
+}
+
+resource "digitalocean_droplet" "jenkins" {
+  name  = "jenkins-droplet"
+  size  = "s-1vcpu-1gb"
+  image = "ubuntu-20-04-x64"
+  region = "nyc1"
+  ssh_keys = ["2c:71:cd:ff:10:06:16:15:3a:60:e3:71:89:56:c2:06"]
+
+}
+
+output "public_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
+}
